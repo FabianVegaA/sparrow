dd92cec606a10e23a4ae968f42fcdd0d501a8cad
FabianVegaA <fabianveal@gmail.com>
2023-02-27 16:08:45 -0300

    tests(base): add base tests

---
+++ tests/base_tests.py
@@ -0,0 +1,43 @@
+import pytest
+
+from src import constant, identity
+
+cases = [
+    (1, 1),
+    ("1", "1"),
+    (True, True),
+    (False, False),
+    (None, None),
+    ([1, 2, 3], [1, 2, 3]),
+    ((1, 2, 3), (1, 2, 3)),
+    ({1, 2, 3}, {1, 2, 3}),
+    ({1: 1, 2: 2, 3: 3}, {1: 1, 2: 2, 3: 3}),
+]
+
+
+@pytest.mark.parametrize("arg, result", cases)
+def test_identity(arg, result):
+    assert identity(arg) == result
+
+
+@pytest.mark.parametrize("arg, result", cases)
+def test_constant(arg, result):
+    assert constant(arg)() == result
+
+
+cases_with_args = [
+    (1, 2, 1),
+    ("1", "2", "1"),
+    (True, False, True),
+    (False, True, False),
+    (None, 1, None),
+    ([1, 2, 3], [4, 5, 6], [1, 2, 3]),
+    ((1, 2, 3), (4, 5, 6), (1, 2, 3)),
+    ({1, 2, 3}, {4, 5, 6}, {1, 2, 3}),
+    ({1: 1, 2: 2, 3: 3}, {4: 4, 5: 5, 6: 6}, {1: 1, 2: 2, 3: 3}),
+]
+
+
+@pytest.mark.parametrize("arg, args, result", cases_with_args)
+def test_constant_with_args(arg, args, result):
+    assert constant(arg)(args) == result
