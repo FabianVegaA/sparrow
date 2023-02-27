fc6b852441ea7fdb56605d94968b4a1feed9fb8a
FabianVegaA <fabianveal@gmail.com>
2023-02-27 18:07:52 -0300

    feat(maybe/data): add maybe functor

---
+++ src/data/maybe.py
@@ -0,0 +1,29 @@
+from abc import ABC
+from dataclasses import dataclass
+from typing import Callable, Generic, TypeVar
+
+from src import T, V
+from src.data.functor import Functor
+
+
+class Maybe(Functor[T], ABC):
+    def fmap(self: "Maybe[T]", f: Callable[[T], V]) -> "Maybe[V]":
+        return self if isinstance(self, Nothing) else Just(f(self.value))
+
+
+@dataclass
+class Just(Maybe[T]):
+    __slots__ = ("value",)
+
+    value: T
+
+    def __repr__(self):
+        return f"Just({self.value})"
+
+
+@dataclass
+class Nothing(Maybe[T]):
+    __slots__ = ()
+
+    def __repr__(self):
+        return "Nothing"
