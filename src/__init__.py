e8a3190709288b83c0f556fe7570f45552a72162
FabianVegaA <fabianveal@gmail.com>
2023-02-28 12:09:50 -0300

    docs(base): add docstrings

--- src/__init__.py
+++ src/__init__.py
@@ -4,10 +4,13 @@ T, V, U, W = TypeVar("T"), TypeVar("V"), TypeVar("U"), TypeVar("W")
 
 
 def identity(x: T) -> T:
+    """Returns the value passed to it."""
     return x
 
 
 def constant(x: T) -> Callable[[...], T]:
+    """Makes a function that always returns the same value."""
+
     def f(*args, **kwargs):
         return x
 
