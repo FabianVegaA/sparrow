26477101ea549646a22499e1dc0b8a8c852fe3a1
FabianVegaA <fabianveal@gmail.com>
2023-02-27 22:59:44 -0300

    feat(wrap/example): add wrapper examples

---
+++ examples/wrap_examples.py
@@ -0,0 +1,20 @@
+from src.decorator.wrap import maybe, result
+from src.decorator.when import when
+from src.data.maybe import Maybe
+
+
+@maybe
+def inverse(x: int) -> Optional[float]:
+    return 1 / x if x != 0 else None
+
+
+value: Maybe[float] = inverse(2)  # Just(0.5)
+value: Maybe[float] = inverse(0)  # Nothing
+
+
+@result
+def inverse(x: int) -> float:
+    return 1 / x
+
+value: Result[float, Exception] = inverse(2)  # Success(0.5)
+value: Result[float, Exception] = inverse(0)  # Failure(ZeroDivisionError('division by zero'))
