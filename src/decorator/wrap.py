0119ebb5b16d9e7373292c842cd76ca4b8e64524
FabianVegaA <fabianveal@gmail.com>
2023-02-28 12:13:20 -0300

    docs(wrap/decorator): add docstrings

--- src/decorator/wrap.py
+++ src/decorator/wrap.py
@@ -7,6 +7,15 @@ from src.data.result import Failure, Result, Success
 
 
 def maybe(f: Callable[..., Optional[T]]) -> Callable[..., Maybe[T]]:
+    """Wraps a function that returns an optional value and returns a Maybe instead.
+
+    Args:
+        f (Callable[..., Optional[T]]): The function to wrap.
+
+    Returns:
+        Callable[..., Maybe[T]]: The wrapped function.
+    """
+
     @wraps(f)
     def wrapper(*args, **kwargs) -> Maybe[T]:
         result = f(*args, **kwargs)
@@ -16,6 +25,15 @@ def maybe(f: Callable[..., Optional[T]]) -> Callable[..., Maybe[T]]:
 
 
 def result(f: Callable[..., T]) -> Callable[..., Result[T, Exception]]:
+    """Wraps a function that returns a value and catches any exceptions, returning a Result instead.
+
+    Args:
+        f (Callable[..., T]): The function to wrap.
+
+    Returns:
+        Callable[..., Result[T, Exception]]: The wrapped function.
+    """
+
     @wraps(f)
     def wrapper(*args, **kwargs) -> Result[T, Exception]:
         try:
