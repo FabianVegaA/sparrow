83658357e0a5a0388a2f4e6ee6a2e643d8a6b08b
FabianVegaA <fabianveal@gmail.com>
2023-02-28 12:13:45 -0300

    docs(when/function): add docstrings

--- src/function/when.py
+++ src/function/when.py
@@ -9,6 +9,17 @@ def when(
     value: T,
     otherwise: Callable[T, T] = identity,
 ) -> Callable[T, T]:
+    """Returns the result of applying the function to the value if the condition is true, otherwise returns the value.
+
+    Args:
+        condition (bool): The condition to check.
+        then (Callable[T, T]): The function to apply to the value if the condition is true.
+        value (T): The value to pass to the function.
+        otherwise (Callable[T, T], optional): The function to apply to the value if the condition is false. Defaults to identity.
+
+    Returns:
+        Callable[T, T]: The result of applying the function to the value if the condition is true, otherwise returns the value.
+    """
     return then(value) if condition else otherwise(value)
 
 
@@ -18,22 +29,55 @@ def unless(
     value: T,
     otherwise: Callable[T, T] = identity,
 ) -> Callable[T, T]:
+    """Returns the result of applying the function to the value if the condition is false, otherwise returns the value.
+
+    Args:
+        condition (bool): The condition to check.
+        then (Callable[T, T]): The function to apply to the value if the condition is false.
+        value (T): The value to pass to the function.
+        otherwise (Callable[T, T], optional): The function to apply to the value if the condition is true. Defaults to identity.
+
+    Returns:
+        Callable[T, T]: The result of applying the function to the value if the condition is false, otherwise returns the value.
+    """
     return when(not condition, then, value, otherwise)
 
 
 def map_when(
     predicate: Callable[T, bool],
-    then: Callable[T, V],
+    then: Callable[T, T],
     value: Sequence[T],
-    otherwise: Callable[T, V] = identity,
-) -> Sequence[V]:
+    otherwise: Callable[T, T] = identity,
+) -> Sequence[T]:
+    """Returns a sequence of the results of applying the function to the value if the condition is true, otherwise returns the value.
+
+    Args:
+        predicate (Callable[T, bool]): The function to check the condition.
+        then (Callable[T, T]): The function to apply to the value if the condition is true.
+        value (Sequence[T]): The value to pass to the function.
+        otherwise (Callable[T, T], optional): The function to apply to the value if the condition is false. Defaults to identity.
+
+    Returns:
+        Sequence[T]: A sequence of the results of applying the function to the value if the condition is true, otherwise returns the value.
+    """
     return map(lambda x: when(predicate(x), then, x, otherwise), value)
 
 
 def map_unless(
     predicate: Callable[T, bool],
-    then: Callable[T, V],
+    then: Callable[T, T],
     value: Sequence[T],
-    otherwise: Callable[T, V] = identity,
-) -> Sequence[V]:
+    otherwise: Callable[T, T] = identity,
+) -> Sequence[T]:
+    """Returns a sequence of the results of applying the function to the value if the condition is false, otherwise returns the value.
+
+    Args:
+        predicate (Callable[T, bool]): The function to check the condition.
+        then (Callable[T, T]): The function to apply to the value if the condition is false.
+        value (Sequence[T]): The value to pass to the function.
+        otherwise (Callable[T, T], optional): The function to apply to the value if the condition is true. Defaults to identity.
+
+    Returns:
+        Sequence[T]: A sequence of the results of applying the function to the value if the condition is false, otherwise returns the value.
+    """
     return map(lambda x: unless(predicate(x), then, x, otherwise), value)
